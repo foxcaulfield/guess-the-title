60f7045930fd09a5896d734ae4c0330e82a66789
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-05 13:32:08 +0300

    Add styled components to project

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -1,13 +1,31 @@
 import React from "react";
 import Title from "../Title/Title";
+import styled from "styled-components";
 
 function Field(props) {
   return (
-    <div>
-      Field
+    <FieldContainer>
       <Title></Title>
-    </div>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+      <Title></Title>
+    </FieldContainer>
   );
 }
 
 export default Field;
+
+let x = 3;
+let y = 3;
+
+const FieldContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(${x}, 1fr);
+  grid-template-rows: repeat(${y}, 1fr);
+  grid-gap: 5px;
+`;
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function Title(props) {
   return (
     <div>
-      <button>Title</button>
+      <Block>Title</Block>
     </div>
   );
 }
 
 export default Title;
+
+const Block = styled.div`
+  background-color: white;
+  color: ${({ color }) => color || "blue"};
+  padding: 10px;
+  border: 1px solid ${({ color }) => color || "blue"};
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightblue;
+  }
+`;
+
+// background-color: dodgerblue;
